95b083fd2d653c96f356d400540bf8c2c93b2b07
ryan <ryanmccarthy667@gmail.com>
2022-04-24 19:57:05 +1200

    changed bash file

--- install.sh
+++ install.sh
@@ -5,4 +5,10 @@ apt-get install python3-pyqt5
 apt-get install pyqt5-dev-tools
 apt-get install qttools5-dev-tools
 apt install opencv-python-headless
+apt update
+apt install gnupg2 software-properties-common apt-transport-https wget
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt update
+sudo apt install code
 
