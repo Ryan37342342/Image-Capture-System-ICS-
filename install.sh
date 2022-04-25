5e08c8522faf334f53331b0bb75de4f700eea816
ryan <ryanmccarthy667@gmail.com>
2022-04-26 00:50:11 +1200

    install.sh now installs pycharm and opens it also installs teamviewer

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
-apt install pip
-apt install python3
+apt install pip -y
+apt install python3 -y
 pip3 install --user pyqt5  
 apt-get install python3-pyqt5  
 apt-get install pyqt5-dev-tools
 apt-get install qttools5-dev-tools
 apt install opencv-python-headless
 apt update
+sudo apt update -y
 pip install numpy
-apt install gnupg2 software-properties-common apt-transport-https wget
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt update
-sudo apt install code
-
+sudo apt install snap -y
+sudo snap install pycharm-community --classic
+wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
+sudo apt install ./teamviewer_amd64.deb -y
+pycharm-community
