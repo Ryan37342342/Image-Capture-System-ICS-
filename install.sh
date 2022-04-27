5b50630e1ff12b9388c54702a0632eb2adbe9ce8
ryan <ryanmccarthy667@gmail.com>
2022-04-28 10:19:49 +1200

    made changes to install script, removed timeviewer and add -y to some commands that required it for auto installation

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 apt install pip -y
 apt install python3 -y
-pip3 install --user pyqt5  
-apt-get install python3-pyqt5  
-apt-get install pyqt5-dev-tools
-apt-get install qttools5-dev-tools
-apt install opencv-python-headless
+pip3 install --user pyqt5  -y
+apt-get install python3-pyqt5 -y
+apt-get install pyqt5-dev-tools -y
+apt-get install qttools5-dev-tools -y
+apt install opencv-python-headless -y
 apt update
 sudo apt update -y
 pip install numpy
 sudo apt install snap -y
 sudo snap install pycharm-community --classic
-wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
-sudo apt install ./teamviewer_amd64.deb -y
+#wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
+#sudo apt install ./teamviewer_amd64.deb -y
 pycharm-community
