cd30749ae8f0b4bc0be328c9b4ae50bc33b623ad
ryan <ryanmccarthy667@gmail.com>
2022-09-09 13:06:03 +1200

    Reset file structure for easier installation with ros

---
+++ ICS/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/aaeon/PycharmProjects/Image-Capture-System-ICS-/ICS/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/aaeon/PycharmProjects/Image-Capture-System-ICS-/ICS/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/aaeon/PycharmProjects/Image-Capture-System-ICS-/ICS/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/aaeon/PycharmProjects/Image-Capture-System-ICS-/ICS/src:$ROS_PACKAGE_PATH"
